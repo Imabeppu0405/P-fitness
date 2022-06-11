f8bd4f5dae3701a9b35b8064269d957b04a0bd9d
Imabeppu0405 <ym07019458069@gmail.com>
2022-06-11 19:31:20 +0900

    簡単なrouter実装

--- work/index.php
+++ work/index.php
@@ -5,10 +5,27 @@ define('PORT', '3306');
 define('DATABASE', 'fitnessdb');
 define('USERNAME', 'd.imabeppu');
 define('PASSWORD', 'fitness');
+require_once __DIR__ . '/php/core/Router.php';
+require_once __DIR__ . '/php/core/Request.php';
+require_once __DIR__ . '/php/core/Application.php';
 require_once __DIR__ . '/php/partials/header.php';
 require_once __DIR__ . '/php/partials/footer.php';
 
+use app\core\Application;
+
+$app = new Application();
+
 \partials\header();
+
+$app->router->get('/', function() {
+  return 'ホームページです。';
+});
+
+$app->router->get('/signin', function() {
+  return 'サインインページです。';
+});
+
+$app->run();
 
 try {
   $db  = new PDO('mysql:host=' . HOSTNAME .';port=' . PORT . ';dbname=' . DATABASE, USERNAME, PASSWORD);
