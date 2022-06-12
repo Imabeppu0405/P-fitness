a19d139cc0bcf9a1ce0aad78a33d1dae7b2e6fd6
Imabeppu0405 <ym07019458069@gmail.com>
2022-06-12 18:48:59 +0900

    DB接続、処理のクラス作成

--- work/php/sql/datasource.php
+++ work/php/sql/datasource.php
@@ -1,28 +1,54 @@
-<?php 
-function db_connection() {
-  $host = "localhost";
-  $port = "8080";
-  $db_database = "fitnessdb";
-  $db_user = "d.imabeppu";
-  $db_password = "fitness";
+<?php
+namespace db;
 
-  try {
-    $conn = new PDO("mysql:host=${host};port={$port};dbname={$db_database}, $db_user, $db_password");
+use PDO;
 
-    $sql = 'CREATE TABLE test (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255),
-      age INT
-    )';
+class DataSource {
 
-    $res = $conn->query($sql);
-
-  } catch(PDOException $e) {
-
-    die();
-  }
+  private $conn;
+  private $result;
+  public const CLS = 'cls';
   
-  $conn = null;
+  public function __construct($host = 'db', $port = '3306', $dbName = 'fitnesdb', $username = 'd.imabeppu', $password = 'fitness')
+  {
+    $dsn = "mysql:host={$host};port={$port};dbname={$dbName};";
+    $this->conn = new PDO($dsn, $username, $password);
+    // 結果を連想配列でとる
+    $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // エラーを詳細に表示
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // 静的プレースホルダにする(SQLインジェクション対策)
+    $this->conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  }
 
-};
-?>
+  private function executeSql($sql, $params)
+  {
+    $stmt = $this->conn->prepare($sql);
+    $this->result = $stmt->execute($params);
+    return $stmt;
+  }
+
+  public function select($sql = "", $params = [], $type = '', $cls = '')
+  {
+    $stmt = $this->executeSql($sql, $params);
+
+    // クラスの形式で取るかどうか
+    if ($type === static::CLS) {
+      return $stmt->fetchAll(PDO::FETCH_CLASS, $cls);
+    } else {
+      return $stmt->fetchAll();
+    }
+  }
+
+  public function execute($sql = "", $params = [])
+  {
+    $this->executeSql($sql, $params);
+    return $this->result;
+  }
+
+  public function selectOne($sql = "", $params = [], $type = '', $cls = '')
+  {
+    $result = $this->select($sql, $params, $type, $cls);
+    return count($result)> 0 ? $result[0] : false;
+  }
+}
