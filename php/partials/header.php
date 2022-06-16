06fdaa389b383f599bed1c01724b2910f1b38cfd
Imabeppu0405 <ym07019458069@gmail.com>
2022-06-17 06:46:06 +0900

    ヘッダー作成

--- php/partials/header.php
+++ php/partials/header.php
@@ -2,6 +2,7 @@
 namespace partials;
 
 use app\core\Message\Msg;
+use app\core\Auth;
 
 function header() {
 ?>
@@ -15,6 +16,20 @@ function header() {
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
+    <header>
+      <nav class="d-flex align-items-center justify-content-between py-2 px-5">
+          <a class="h2 font-weight-bold" href="<?php the_url('/') ?>">P-fitness</a>
+          <div>
+            <?php if(Auth::isLogin()): ?>
+            <a href="<?php the_url('/') ?>" class="mx-3 h5 pt-2">Home</a>
+            <a href="<?php the_url('logout') ?>" class="btn btn-danger" >ログアウト</a>
+            <?php else: ?>
+            <a href="<?php the_url('signup') ?>" class="btn btn-primary mx-3">登録</a>
+            <a href="<?php the_url('signin') ?>" >ログイン</a>
+            <?php endif; ?>
+          </div>
+      </nav>
+    </header>
 <?php
 Msg::flush(); 
 }
