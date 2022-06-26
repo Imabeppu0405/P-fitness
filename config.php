e30a01ad9c0de1bc5d2d3f19e3c42c45e26831a4
Imabeppu0405 <ym07019458069@gmail.com>
2022-06-27 07:24:46 +0900

    404でのエラー表示、デバッグモード解除

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('SOURCE_BASE', __DIR__ . '/php/');
 define('CURRENT_URI', $_SERVER{'REQUEST_URI'});
-define('DEBUG', true);
+define('DEBUG', false);
 define('GO_HOME', 'home');
 define('GO_REFERER', 'referer');
--- php/views/404.php
+++ php/views/404.php
@@ -1,3 +1,8 @@
+<?php
+
+use app\core\Message\Msg;
+
+ Msg::flush(); ?>
 <div class="text-center">
   <h1 class="display-1 mt-5">404</h1>
   <h3>ページが見つかりません。</h3>
